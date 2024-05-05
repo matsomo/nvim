260f53fe1ae1838701d2ec520ada88bf65b81ddb
Mats Aadahl <mats.aadahl@esmartsystems.com>
2024-05-05 12:38:45 +0200

    fixed broken map in trouble.lua

--- lua/matsa/plugins/trouble.lua
+++ lua/matsa/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
 	keys = {
-		{ "leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
+		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
 		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
 		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
 		{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
