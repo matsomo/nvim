2b32cddbaa1a0330add42e9ba1bb8fb3b8954f8d
Mats Aadahl <mats.aadahl@esmartsystems.com>
2025-10-26 12:52:20 +0100

    codecompanion and related tooling, updated dependencies

---
+++ lua/matsa/plugins/codecompanion.lua
@@ -0,0 +1,45 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	cmd = {
+		"CodeCompanion",
+		"CodeCompanionChat",
+		"CodeCompanionActions",
+		"CodeCompanionToggle",
+		"CodeCompanionAdd",
+	},
+	keys = {
+		{
+			"<leader>cc",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			desc = "CodeCompanion - Toggle chat",
+			mode = { "n", "v" },
+		},
+	},
+	opts = {
+		strategies = {
+			chat = {
+				name = "copilot",
+				model = "gpt-4.1",
+			},
+		},
+		-- NOTE: The log_level is in `opts.opts`
+		opts = {
+			log_level = "DEBUG",
+		},
+		display = {
+			chat = {
+				window = {
+					layout = "vertical", -- or "horizontal", "float"
+					width = 0.45,
+					height = 0.8,
+					relative = "editor",
+					border = "rounded",
+				},
+			},
+		},
+	},
+}
