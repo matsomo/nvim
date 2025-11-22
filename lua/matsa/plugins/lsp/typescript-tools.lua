bd8478e01633eb7e633739fd6cf583513a48e2fa
Mats Aadahl <mats.aadahl@esmartsystems.com>
2025-11-22 11:33:38 +0100

    tsgo server path

--- lua/matsa/plugins/lsp/typescript-tools.lua
+++ lua/matsa/plugins/lsp/typescript-tools.lua
@@ -2,6 +2,9 @@ return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	config = function()
-		require("typescript-tools").setup({})
+		require("typescript-tools").setup({
+			-- update to this when tsgo supports import auto-imports
+			-- tsserver_path = "node_modules/@typescript/native-preview/native-preview-darwin-arm64/bin/typescript-language-server",
+		})
 	end,
 }
