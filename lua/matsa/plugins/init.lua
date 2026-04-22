6014b6fb9568f3c5ad5b2ca006009a0375a71a70
Mats Aadahl <mats.aadahl@esmartsystems.com>
2026-04-22 10:30:21 +0200

    fixed leader + e widening selection not working

--- lua/matsa/plugins/init.lua
+++ lua/matsa/plugins/init.lua
@@ -1,4 +1,11 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions used by many plugins
-  "christoomey/vim-tmux-navigator" -- tmux & split window navigation
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "daliusd/incr.nvim",
+    opts = {
+      incr_key = "<leader>e",
+      decr_key = "<bs>",
+    },
+  },
 }
